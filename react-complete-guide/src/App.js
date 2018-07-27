f7688e5cb3a0fc1e5acbef8ae9e8432f7236ba98
Duy Nguyen <BZB87Q@NAM.corp.gm.com>
2018-07-27 10:02:43 -0500

    Error Boundry

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 // import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -45,50 +46,42 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
-  render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-    
+  render() {    
     let persons = null;
+    let btnClass ='';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return <ErrorBoundary key={person.id}>
+              <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}              
+              changed={(event) => this.nameChangedHandler(event, person.id)} /> 
+            </ErrorBoundary>
           })}
         </div>
       );
-
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
     
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
-            style={style}
-            onClick={this.togglePersonHandler}>Switch Name</button>
+            className={btnClass}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
           {persons}
         </div>
     );
