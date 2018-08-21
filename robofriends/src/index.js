e4b070be29cd736beea5dce7f5598a359c442dd8
Drake Nguyen <drakehanguyen@gmail.com>
2018-08-20 22:12:03 -0500

    Finish lesson 160

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello.js';
 import registerServiceWorker from './registerServiceWorker';
+import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
 registerServiceWorker();
