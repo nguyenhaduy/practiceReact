f6b3daa2b31b3535f60e1b1c3dbe3ad106effa17
Duy Nguyen <drake.ha.nguyen@gmail.com>
2018-10-30 15:02:49 -0500

    Finished Lesson 185

--- robofriends/src/components/CardList.js
+++ robofriends/src/components/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    if (false) {
+        throw new Error('NOOOO!');
+    }
     return (
         <div>
             {
--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,45 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Cardlist from "../components/CardList";
 import SearchBox from'../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
+import { setSearchField, requestRobots } from '../actions'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
 class App extends Component{
-    constructor () {
-        super ();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
-
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {this.setState({ robots: users})});
+        this.props.onRequestRobots();
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-        
-    }
     render () {
-        const {robots, searchfield} = this.state;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        return !robots.length? 
+        return isPending ? 
             <h1>Loading</h1> :
             (            
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
-                        <Cardlist robots={filterRobots} />
+                        <ErrorBoundry>
+                            <Cardlist robots={filterRobots} />
+                        </ErrorBoundry>                        
                     </Scroll>
                 </div>        
             );
     }    
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
